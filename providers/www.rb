1120d006cc04455c1953288d21d86163ba5760fe
Jean-Francois Theroux <failshell@gmail.com>
2013-05-11 13:19:33 -0400

    fixed syntax

--- providers/www.rb
+++ providers/www.rb
@@ -8,11 +8,11 @@ action :fetch do
     # To make our life easier, we don't care if the certificate is not ok
     command "wget -N --no-check-certificate #{new_resource.url} -P #{new_resource.dest}"
     unless new_resource.overwrite == true
-      creates "#{new_resource.dest}/#{IO::File.basename(new_resource.url)}"
+      creates "#{new_resource.dest}/#{::File.basename(new_resource.url)}"
     end
   end
 
-  file "#{new_resource.dest}/#{IO::File.basename(new_resource.url)}" do
+  file "#{new_resource.dest}/#{::File.basename(new_resource.url)}" do
     if new_resource.owner
       owner new_resource.owner
     end
